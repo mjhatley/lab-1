2ef7efecc8fafced140f53ebfd8e58b339e2d901
mjhatley <mjhatley21@email.com>
2018-01-23 13:00:31 -0500

    lab 1

---
+++ Grand Circus Labs/lab 1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab_Number_one
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+            Console.WriteLine("Enter Length:");
+            Double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Width:");
+            Double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Area:");
+            Console.WriteLine(firstNumber * secondNumber);
+
+            Console.WriteLine("Perimeter:");
+            Console.WriteLine((2 * firstNumber) + (2 * secondNumber));
+
+
+            Console.WriteLine("Continue (y/n)");
+
+            if (Console.ReadKey().Key == ConsoleKey.N)
+                return;
+            Console.WriteLine(" ");
+
+
+
+            Console.WriteLine("Enter Length:");
+            Double fifthNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Width:");
+            Double sixthNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Area");
+            Console.WriteLine(fifthNumber * sixthNumber);
+
+                   
+            Console.WriteLine("Perimeter");
+            Console.WriteLine((2 * fifthNumber) + (2 * sixthNumber));
+
+
+
+            
+        }
+    }
